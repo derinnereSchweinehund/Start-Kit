4787f72c77753f62d3207f2bffd41542f0cd459a
AnthonyZhOon <126740410+AnthonyZhOon@users.noreply.github.com>
2024-02-20 15:03:33 +1100

    Refactored ActionSimulator.hpp (#3)
    * Move implementation to .cpp file for ActionSimulator.hpp
    
    * Refactored ActionSimulator to implement Perfect and Probabilistic in cpp file

--- inc/ActionSimulator.hpp
+++ inc/ActionSimulator.hpp
@@ -1,77 +1,45 @@
 #include "ActionModel.h"
 #include "SharedEnv.h"
 #include "Status.hpp"
+#include <boost/asio/io_service.hpp>
 #include <random>
 
-class ActionSimulator {
+template <class ActionModel> class ActionSimulator {
 public:
-  ActionSimulator(ActionModelWithRotate &model, SharedEnvironment *env)
+  ActionSimulator(ActionModel &model, SharedEnvironment *env)
       : model(model), env(env){};
   virtual vector<Status> simulate_action(vector<Action> &next_actions);
   virtual bool validate_safe(const vector<Action> &next_actions);
 
 protected:
-  ActionModelWithRotate &model;
+  ActionModel &model;
   SharedEnvironment *env;
 };
 
 // Classical MAPF scenario where all actions succeed
-class PerfectSimulator : ActionSimulator {
+// I wanted to write a template <class T> to do ActionSimulator<T>  
+class PerfectSimulator : ActionSimulator<ActionModelWithRotate> {
 public:
   PerfectSimulator(ActionModelWithRotate &model, SharedEnvironment *env)
       : ActionSimulator(model, env){};
 
-  vector<Status> simulate_action(vector<Action> &next_actions) override {
-    env->curr_states = model.result_states(env->curr_states, next_actions);
-    return vector<Status>(env->num_of_agents, Status::SUCCESS);
-  }
+  vector<Status> simulate_action(vector<Action> &next_actions) override;
 
-  bool validate_safe(const vector<Action> &next_actions) override {
-    vector<State> next_states = model.result_states(env->curr_states, next_actions);
-    // Check vertex conflicts
-    for (int i = 0; i < env->num_of_agents; i++) {
-      for (int j = i + 1; j < env->num_of_agents; j++) {
-        if (next_states.at(i).location == next_states.at(j).location) {
-          return false;
-        }
-      }
-    }
-    // Check for edge conflicts
-    // If current and next state coincide in direction
-    for (int i = 0; i < env->num_of_agents; i++) {
-      for (int j = 0; j < env->num_of_agents; i++) {
-        if (next_states.at(i).location == env->curr_states.at(j).location && next_states.at(j).location == env->curr_states.at(i).location){
-          return false;
-        } 
-      }
-    }
-    return true;
-  }
+  bool validate_safe(const vector<Action> &next_actions) override;
 
 };
 
 // Implements delay probability for MAPF-DP
-class ProbabilisticSimulator : ActionSimulator {
+class ProbabilisticSimulator : ActionSimulator<ActionModelWithRotate> {
 public:
   ProbabilisticSimulator(float success_chance, ActionModelWithRotate &model,
                          SharedEnvironment *env)
       : success_chance_(success_chance), rd_(), gen_(rd_()), distrib_(0, 1),
         ActionSimulator(model, env){};
 
-  vector<Status> simulate_action(vector<Action> &next_actions) override {
-    vector<Status> progress(env->num_of_agents);
-    for (int i = 0; i < env->num_of_agents; i++) {
-      // Succeeds success_chance % of the time, never if <=0 and always if >=1
-      if (success_chance_ > distrib_(gen_)) {
-        progress[i] = Status::SUCCESS;
-        State &curr = env->curr_states.at(i);
-        curr = model.result_state(curr, next_actions.at(i));
-      } else {
-        progress[i] = Status::FAILED;
-      }
-    }
-    return progress;
-  }
+  vector<Status> simulate_action(vector<Action> &next_actions) override;
+
+  virtual bool validate_safe(const vector<Action> &next_actions) override;
 
 private:
   float success_chance_;
