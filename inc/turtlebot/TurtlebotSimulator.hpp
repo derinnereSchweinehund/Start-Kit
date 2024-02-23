0afb83e53bb8e8ed853b26c6b1ad196a89d50ade
Anthony Oon <anthony.zh.oon@gmail.com>
2024-02-23 14:01:55 +1100

    Removed inheritance in TurtlebotSimulator, now standalone template class

--- inc/turtlebot/TurtlebotSimulator.hpp
+++ inc/turtlebot/TurtlebotSimulator.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "ActionModel.h"
-#include "ActionSimulator.hpp"
 #include "FreeState.h"
 #include "Grid.h"
 #include "SharedEnv.h"
@@ -25,18 +24,47 @@ namespace http = beast::http;
 using json = nlohmann::json;
 using tcp = boost::beast::net::ip::tcp;
 
-class TurtlebotSimulator : ActionSimulator<ActionModelWithRotate> {
+template <class ActionModel>
+class TurtlebotSimulator {
 public:
   TurtlebotSimulator(ActionModelWithRotate &model, Grid &grid)
-      : stream_(ioc_), resolver_(ioc_), ActionSimulator(model, grid) {}
+      : stream_(ioc_), resolver_(ioc_),
+        model_(model), grid_(grid) {}
 
   void simulate_action(SharedEnvironment &state,
-                                 const vector<Action> &next_actions) override;
+                       const vector<Action> &next_action) {
+    if (!validate_safe(state, next_action)) {
+      for (Status agent_status : state.current_status_) {
+        agent_status = Status::FAILED;
+      }
+      return;
+    }
+
+    vector<State> next_states =
+        model_.result_states(state.current_states_, next_action);
+
+    http::response<http::dynamic_body> res = send_next_states(next_states);
+
+    assert(res.base().result_int() == 200); // Assert successful http
+
+    // Understand response to create Status codes
+    json agent_results = get_agent_status();
+
+    state.current_states_ = parseStates(agent_results["locations"],
+                                        state.timestep_, state.num_of_agents_);
+    state.current_status_ =
+        parseStatus(agent_results["status"], state.num_of_agents_);
+    return;
+  }
 
   bool validate_safe(const SharedEnvironment &state,
-                     const vector<Action> &next_actions) override;
+                     const vector<Action> &next_actions) {
+    return validate_unfailing(next_actions, state, model_);
+  }
 
 private:
+  ActionModel model_;
+  Grid grid_;
   const string hostname;
   const string port;
   boost::asio::io_service ioc_;
@@ -45,6 +73,59 @@ private:
 
   const string post_path = "/extend_path";
   const string get_path = "/get_status";
+
+  // Check for vertex and edge conflicts
+  bool validate_unfailing(const vector<Action> &next_actions,
+                          const SharedEnvironment &state) {
+    vector<State> next_states =
+        model_.result_states(state.current_states_, next_actions);
+    // Check vertex conflicts
+    for (int i = 0; i < state.num_of_agents_; i++) {
+      for (int j = i + 1; j < state.num_of_agents_; j++) {
+        if (next_states.at(i).location == next_states.at(j).location) {
+          return false;
+        }
+      }
+    }
+    // Check for edge conflicts
+    // If current and next state coincide in direction
+    for (int i = 0; i < state.num_of_agents_; i++) {
+      for (int j = 0; j < state.num_of_agents_; i++) {
+        if (next_states.at(i).location ==
+                state.current_states_.at(j).location &&
+            next_states.at(j).location ==
+                state.current_states_.at(i).location) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  bool validate_failing(const vector<Action> &next_actions,
+                        const SharedEnvironment &state) {
+    // Check for vertex and edge conflicts
+    assert(next_actions.size() == state.num_of_agents_);
+    if (!validate_unfailing(next_actions, state, model_)) {
+      return false;
+    }
+    // Check for 1-robustness
+    unordered_set<int> occupied_before;
+    const vector<State> &current_states_ = state.current_states_;
+    for (int i = 0; i < state.num_of_agents_; i++) {
+      occupied_before.insert({current_states_[i].location});
+    }
+    vector<State> next_states =
+        model_.result_states(current_states_, next_actions);
+    for (int i = 0; i < state.num_of_agents_; i++) {
+      auto res = occupied_before.find(next_states[i].location);
+      if (res != occupied_before.end()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 
   inline float location_to_x(int location, const Grid &grid) {
     return static_cast<float>(location % grid.cols);
@@ -84,7 +165,7 @@ private:
     json plans = json::array();
 
     for (int i = 0; i < num_of_agents; i++) {
-      FreeState next = transform_state(next_states.at(i), grid);
+      FreeState next = transform_state(next_states.at(i), grid_);
       plans.push_back(stateToJson(i, next));
     }
 
@@ -124,7 +205,7 @@ private:
   }
 
   http::response<http::dynamic_body>
-  send_next_states(const vector<State> &next_states) {
+  send_next_states(SharedEnvironment *state, const vector<State> &next_states) {
 
     json payload = construct_payload(next_states, state->num_of_agents_);
 
@@ -144,7 +225,7 @@ private:
     http::request<http::string_body> req;
     fill_request(req, http::verb::get, get_path, json::object());
     http::response<http::dynamic_body> res;
-    
+
     // This will attempt for 5 minutes
     for (int i = 0; i < 500; i++) {
       int response_code = send_request(req, res);
@@ -153,12 +234,12 @@ private:
         sleep(1);
       } else {
         break;
-      } 
+      }
     }
     assert(res.base().result_int() == 200); // Assert that the request succeeded
 
     // data has 2 fields {
-    //              "locations" : [{x, y, theta, agent_id}], 
+    //              "locations" : [{x, y, theta, agent_id}],
     //              "status" : [SUCEEDED|FAILED|IN-PROGRESS]
     //              }
 
@@ -171,11 +252,12 @@ private:
     int theta = state_json["theta"].get<int>();
     int agent_id = state_json["agent_id"].get<int>();
 
-    return State(xy_to_location(x, y, grid.cols), timestep,
+    return State(xy_to_location(x, y, grid_.cols), timestep,
                  ((theta + 45) / 90) % 4); // Map 0-360 to 0-3
   }
 
-  vector<State> parseStates(json::array_t json_states, int timestep, int num_of_agents) {
+  vector<State> parseStates(json::array_t json_states, int timestep,
+                            int num_of_agents) {
     vector<State> curr_states(num_of_agents);
     for (int i = 0; i < json_states.size(); i++) {
       json agent_state = json_states[i];
--- src/turtlebot/TurtlebotSimulator.cpp
+++ src/turtlebot/TurtlebotSimulator.cpp
@@ -1,44 +1,45 @@
-#include "turtlebot/TurtlebotSimulator.hpp"
-#include "SharedEnv.h"
-#include "nlohmann/json.hpp"
-#include <boost/beast/http.hpp>
-#include <boost/beast/http/dynamic_body.hpp>
-#include <boost/beast/http/string_body.hpp>
-#include <vector>
+// #include "turtlebot/TurtlebotSimulator.hpp"
+// #include "SharedEnv.h"
+// #include "nlohmann/json.hpp"
+// #include <boost/beast/http.hpp>
+// #include <boost/beast/http/dynamic_body.hpp>
+// #include <boost/beast/http/string_body.hpp>
+// #include <vector>
 
-using json = nlohmann::json;
-namespace beast = boost::beast; // from <boost/beast.hpp>
-namespace http = beast::http;   // from <boost/beast/http.hpp>
+// using json = nlohmann::json;
+// namespace beast = boost::beast; // from <boost/beast.hpp>
+// namespace http = beast::http;   // from <boost/beast/http.hpp>
 
-inline bool
-TurtlebotSimulator::validate_safe(const SharedEnvironment &state,
-                                  const vector<Action> &next_actions) {
-  return validate_unfailing(next_actions, state, model);
-}
+// template<class ActionModel>
+// inline bool
+// TurtlebotSimulator<ActionModel>::validate_safe(const SharedEnvironment &state,
+//                                   const vector<Action> &next_actions) {
+//   return validate_unfailing(next_actions, state, model);
+// }
 
-// TODO: Make this function mutate the shared state status
-void TurtlebotSimulator::simulate_action(SharedEnvironment &state,
-                                         const vector<Action> &next_action) {
-  if (!validate_safe(state, next_action)) {
-    for (Status agent_status : state.current_status_) {
-      agent_status = Status::FAILED;
-    }
-    return;
-  }
+// template<class ActionModel>
+// void TurtlebotSimulator<ActionModel>::simulate_action(SharedEnvironment &state,
+//                                          const vector<Action> &next_action) {
+//   if (!validate_safe(state, next_action)) {
+//     for (Status agent_status : state.current_status_) {
+//       agent_status = Status::FAILED;
+//     }
+//     return;
+//   }
 
-  vector<State> next_states =
-      model.result_states(state.current_states_, next_action);
+//   vector<State> next_states =
+//       model.result_states(state.current_states_, next_action);
 
-  http::response<http::dynamic_body> res = send_next_states(next_states);
+//   http::response<http::dynamic_body> res = send_next_states(next_states);
 
-  assert(res.base().result_int() == 200); // Assert successful http
+//   assert(res.base().result_int() == 200); // Assert successful http
 
-  // Understand response to create Status codes
-  json agent_results = get_agent_status();
+//   // Understand response to create Status codes
+//   json agent_results = get_agent_status();
 
-  state.current_states_ = parseStates(agent_results["locations"],
-                                      state.timestep_, state.num_of_agents_);
-  state.current_status_ =
-      parseStatus(agent_results["status"], state.num_of_agents_);
-  return;
-}
+//   state.current_states_ = parseStates(agent_results["locations"],
+//                                       state.timestep_, state.num_of_agents_);
+//   state.current_status_ =
+//       parseStatus(agent_results["status"], state.num_of_agents_);
+//   return;
+// }
